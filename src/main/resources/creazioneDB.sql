a7a1cc6ab26d6ba8e4540860ca3fd5d8719a19b4
mmongelli5 <mmongelli@studenti.uninsubria.it>
2026-08-09 01:12:02 +0200

    aggiunta tabella Recensioni al DB

--- src/main/resources/creazioneDB.sql
+++ src/main/resources/creazioneDB.sql
@@ -1,9 +1,22 @@
-CREATE TABLE Utenti (
-    username varchar(50) PRIMARY KEY,
-    nome varchar(30),
-    cognome varchar(30),
-    password varchar(300),
-    domicilio varchar(30),
+CREATE TABLE Utenti
+(
+    username     varchar(50) PRIMARY KEY,
+    nome         varchar(30),
+    cognome      varchar(30),
+    password     varchar(300),
+    domicilio    varchar(30),
     data_nascita date,
-    ruolo varchar (11) CHECK (ruolo IN ('Cliente', 'Ristoratore'))
+    ruolo        varchar(11) CHECK (ruolo IN ('Cliente', 'Ristoratore'))
+)
+
+CREATE TABLE Recensioni
+(
+    id_recensione INT PRIMARY KEY NOT NULL,
+    testo         VARCHAR(250),
+    stelle        INT             NOT NULL CHECK (stelle >= 1 AND stelle <= 5),
+    risposta      VARCHAR(250),
+    id_ristorante INT             NOT NULL,
+    username      VARCHAR(50)     NOT NULL,
+    FOREIGN KEY (id_ristorante) REFERENCES Ristoranti (id_ristorante),
+    FOREIGN KEY (username) REFERENCES Clienti (username)
 )
