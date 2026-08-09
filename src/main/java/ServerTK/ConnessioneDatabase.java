36e71209bdabeb81a9700bbdd81667baa9cd7ec4
AuroraSarno <asarno1@studenti.uninsubria.it>
2026-08-09 11:51:38 +0200

    aggiunta classe per connessione database

---
+++ src/main/java/ServerTK/ConnessioneDatabase.java
@@ -0,0 +1,22 @@
+package ServerTK;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnessioneDatabase {
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static String dbUser = "";
+    private static String dbPassword = "";
+
+    //metodo iniziale per definire le credenziali della connessione al database
+    public static void setCredenziali(String username, String password) {
+        dbUser = username;
+        dbPassword = password;
+    }
+
+    //metodo per generare una connessione al database
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, dbUser, dbPassword);
+    }
+}
