e6678efaa54ba405233966ff36946b9b66571ec4
AuroraSarno <asarno1@studenti.uninsubria.it>
2026-08-13 15:02:59 +0200

    Aggiunta classe utente e gestoreUtenti con metodi per comunicare con DB

---
+++ src/main/java/ServerTK/GestoreUtenti.java
@@ -0,0 +1,55 @@
+package ServerTK;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import ServerTK.Utente.Ruolo;
+
+public class GestoreUtenti {
+    //campi
+    //per mandare query a database
+    ConnessioneDatabase connessioneDatabase;
+    public GestoreUtenti(ConnessioneDatabase cD) {
+        connessioneDatabase = cD;
+    }
+
+    //metodi
+    //metodo per inserire utente nella tabella Utenti
+    public boolean registrazione(String username, String nome, String cognome, String password, String domicilio, Date data, Ruolo ruolo) {
+        String query= "INSERT into Utenti VALUES(?,?,?,?,?,?,?)";
+        try(Connection connection=connessioneDatabase.getConnection();) {
+            PreparedStatement statement=connection.prepareStatement(query);
+            statement.setString(1, username);
+            statement.setString(2, nome);
+            statement.setString(3, cognome);
+            statement.setString(4, password);
+            statement.setString(5, domicilio);
+            statement.setDate(6, (java.sql.Date) data);
+            statement.setString(7, ruolo.name());
+            int righeModificate = statement.executeUpdate(query);
+            return righeModificate > 0;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+    }
+
+    public Utente login(String username, String password) {
+        String query= "SELECT * FROM Utenti WHERE username = ? AND password = ?";
+        try(Connection connection= connessioneDatabase.getConnection();
+            PreparedStatement statement= connection.prepareStatement(query);){
+            statement.setString(1,username);
+            statement.setString(2,password);
+            ResultSet resultSet= statement.executeQuery();
+            if(resultSet.next()){
+                return new Utente(resultSet.getString("username"),resultSet.getString("password"),resultSet.getString("nome"),resultSet.getString("cognome"),resultSet.getString("domicilio"),resultSet.getDate("data"),Ruolo.valueOf(resultSet.getString("ruolo")));
+            }else {return null;}
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ src/main/java/ServerTK/Utente.java
@@ -0,0 +1,69 @@
+package ServerTK;
+
+import java.util.Date;
+
+public class Utente {
+    public enum Ruolo{
+        CLIENTE, RISTORATORE
+    }
+    private String username;
+    private String password;
+    private String nome;
+    private String cognome;
+    private String domicilio;
+    private Date date;
+    private Ruolo ruolo;
+
+    public Utente(String username, String password, String nome, String cognome, String domicilio, Date date, Ruolo ruolo) {
+        this.username = username;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.domicilio = domicilio;
+        this.date = date;
+        this.ruolo = ruolo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCognome() {
+        return cognome;
+    }
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    public String getDomicilio() {
+        return domicilio;
+    }
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public Ruolo getRuolo() {
+        return ruolo;
+    }
+    public void setRuolo(Ruolo ruolo) {
+        this.ruolo = ruolo;
+    }
+}
