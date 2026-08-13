a5b72c8f015be9891a1b578f90bb89a129816933
Simone <smalacrida2@studenti.uninsubria.it>
2026-08-13 20:44:24 +0200

    Creazione GestoreRistorante e oggetto Ristorante

---
+++ src/main/java/ServerTK/GestoreRistorante.java
@@ -0,0 +1,202 @@
+package ServerTK;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestoreRistorante{
+
+    //Metodi
+
+    //Ricerca ristorante con filtro
+    //Capire se fare ricerca anche tramite nome ristorante
+    public List<Ristorante> cercaRistoranti(String nome, String tipoCucina, Double prezzoMin, Double prezzoMax,
+                                            Boolean delivery, Boolean prenotazione, Double stelle, Integer idCitta){
+
+        List<Ristorante> risultati = new ArrayList<>();
+
+        StringBuilder sql = new StringBuilder("SELECT * FROM Ristoranti WHERE 1=1");
+        List<Object> valoriParametri = new ArrayList<>();
+
+        if(nome != null && !nome.trim().isEmpty()){
+            sql.append(" AND nome ILIKE = ?");
+            valoriParametri.add(nome);
+        }
+
+        if(tipoCucina != null && !tipoCucina.trim().isEmpty()){
+            sql.append(" AND tipoCucina = ?");
+            valoriParametri.add(tipoCucina);
+        }
+
+        if(prezzoMin != null){
+            sql.append(" AND prezzoMedio >= ?");
+            valoriParametri.add(prezzoMin);
+        }
+
+        if(prezzoMax != null){
+            sql.append(" AND prezzoMedio <= ?");
+            valoriParametri.add(prezzoMax);
+        }
+
+        if(delivery != null){
+            sql.append(" AND delivery = ?");
+            valoriParametri.add(delivery);
+        }
+
+        if(prenotazione != null){
+            sql.append(" AND prenotazione = ?");
+            valoriParametri.add(prenotazione);
+        }
+
+        if(stelle != null){
+            sql.append(" AND stelle >= ?");
+            valoriParametri.add(stelle);
+        }
+
+        if(idCitta != null){
+            sql.append(" AND idCitta = ?");
+            valoriParametri.add(idCitta);
+        }
+
+        try(Connection con = ConnessioneDatabase.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql.toString())) {
+
+            for(int i = 0; i < valoriParametri.size(); i++){
+                ps.setObject(i+1, valoriParametri.get(i));
+            }
+
+            try(ResultSet rs = ps.executeQuery()){
+                while(rs.next()){
+                    Ristorante r = new Ristorante(
+                            rs.getInt("id_ristorante"),
+                            rs.getString("nome"),
+                            rs.getString("indirizzo"),
+                            rs.getDouble("latitudine"),
+                            rs.getDouble("longitudine"),
+                            rs.getInt("prezzoMedio"),
+                            rs.getBoolean("delivery"),
+                            rs.getBoolean("prenotazione"),
+                            rs.getString("tipoCucina"),
+                            (Double) rs.getObject("stelle"),
+                            rs.getString("usernameRistoratore"),
+                            rs.getInt("id_citta")
+                    );
+
+                    risultati.add(r);
+
+                }
+            }
+
+
+        }catch(SQLException e){
+            System.out.println("Errore durante la ricerca: " + e.getMessage());
+        }
+        return risultati;
+    }
+
+    //Inserisci un ristorante nella tabella
+    public boolean aggiungiRistorante(Ristorante r) {
+        String sql = "INSERT INTO Ristoranti (nome, indirizzo, latitudine, longitudine, prezzoMedio, " +
+                "delivery, prenotazione, tipoCucina, stelle, usernameRistoratore, id_citta) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+
+        try(Connection con = ConnessioneDatabase.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1,r.getNome());
+            ps.setString(2,r.getIndirizzo());
+            ps.setDouble(3,r.getLatitudine());
+            ps.setDouble(4,r.getLongitudine());
+            ps.setInt(5, r.getPrezzoMedio());
+            ps.setBoolean(6,r.getDelivery());
+            ps.setBoolean(7,r.getPrenotazione());
+            ps.setString(8, r.getTipoCucina());
+            ps.setObject(9, r.getStelle());
+            ps.setString(10, r.getProprietario());
+            ps.setInt(11, r.getId_citta());
+
+
+            int righeModificate = ps.executeUpdate();
+
+            return righeModificate > 0;
+
+        }catch(SQLException e){
+            System.out.println("Errore durante l'inserimento: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Restituisce una lista di ristoranti di proprietà di uno specifico utente.
+     * @param usernameProprietario Lo username del proprietario dei ristoranti da cercare.
+     * @return Una lista di oggetti <code>Ristorante</code> appartenenti al proprietario specificato.
+     */
+    public List<Ristorante> getRistoranteDi(String usernameProprietario){
+        List<Ristorante> risultati = new ArrayList<>();
+
+        String sql = "SELECT * FROM Ristoranti WHERE usernameRistoratore = ?";
+
+        try(Connection con = ConnessioneDatabase.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, usernameProprietario);
+
+            try(ResultSet rs = ps.executeQuery()){
+                while(rs.next()){
+                    Ristorante r = new Ristorante(
+                            rs.getInt("id_ristorante"),
+                            rs.getString("nome"),
+                            rs.getString("indirizzo"),
+                            rs.getDouble("latitudine"),
+                            rs.getDouble("longitudine"),
+                            rs.getInt("prezzoMedio"),
+                            rs.getBoolean("delivery"),
+                            rs.getBoolean("prenotazione"),
+                            rs.getString("tipoCucina"),
+                            (Double) rs.getObject("stelle"),
+                            rs.getString("usernameRistoratore"),
+                            rs.getInt("id_citta")
+                    );
+
+                    risultati.add(r);
+                }
+            }
+        }catch(SQLException e){
+            System.out.println("Errore durante cerca ristorante di " + usernameProprietario + ": " + e.getMessage());
+        }
+        return risultati;
+    }
+
+    /**
+     * Restituisce una lista di tutte le tipologie di cucina presenti nel sistema, senza duplicati.
+     * @return Una lista di stringhe rappresentanti i tipi di cucina unici.
+     */
+    public List<String> getTipiCucinaLista(){
+        List<String> tipiCucina = new ArrayList<>();
+
+        String sql = "SELECT DISTINCT tipoCucina FROM Ristoranti";
+
+        try(Connection con = ConnessioneDatabase.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql)) {
+
+            try(ResultSet rs = ps.executeQuery()){
+                while(rs.next()){
+                String tC = rs.getString("tipoCucina");
+
+                tipiCucina.add(tC);
+                }
+            }
+
+        }catch(SQLException e){
+            System.out.println("Errore durante cerca tipi cucina: " + e.getMessage());
+        }
+
+        return tipiCucina;
+
+    }
+
+}
+
---
+++ src/main/java/ServerTK/Ristorante.java
@@ -0,0 +1,269 @@
+package ServerTK;
+
+public class Ristorante {
+    //campi
+    private int id_ristorante;
+    /**
+     * Nome del ristorante.
+     * Memorizzato nell'attributo <code>nome</code>.
+     */
+    private String nome;
+
+    /** Indirizzo stradale del ristorante.
+     * Memorizzato nell'attributo <code>indirizzo</code>.
+     */
+    private String indirizzo;
+
+    /** Latitudine geografica del ristorante.
+     * Memorizzato nell'attributo <code>latitudine</code>.
+     */
+    private double latitudine;
+
+    /** Longitudine geografica del ristorante.
+     * Memorizzato nell'attributo <code>longitudine</code>.
+     */
+    private double longitudine;
+
+    /** Prezzo medio di spesa a persona nel ristorante.
+     * Memorizzato nell'attributo <code>prezzoMedio</code>.
+     */
+    private int prezzoMedio;
+
+    /** Indica se il ristorante offre servizio di consegna a domicilio.
+     * Memorizzato nell'attributo <code>delivery</code>.
+     */
+    private Boolean delivery;
+
+    /** Indica se il ristorante accetta prenotazioni online.
+     * Memorizzato nell'attributo <code>prenotazione</code>.
+     */
+    private Boolean prenotazione;
+
+    /** Tipo di cucina offerta dal ristorante(Italiana, cinese, ecc.).
+     * Memorizzato nell'attributo <code>tipoCucina</code>.
+     */
+    private String tipoCucina;
+
+    /** Media della valutazione del ristorante in stelle.
+     * Memorizzato nell'attributo <code>stelle</code>.
+     */
+    private Double stelle;
+
+    /** Nome del proprietario del ristorante.
+     * Memorizzato nell'attributo <code>proprietario</code>.
+     */
+    private String proprietario;
+
+    private int id_citta;
+
+    //costruttore
+
+    /**
+     * Costruisce un nuovo oggetto <code>Ristorante</code> inizializzando tutti i suoi campi.
+     * @param nome  Il nome del ristorante.
+     * @param nazione  La nazione di appartenenza.
+     * @param citta  La città di appartenenza.
+     * @param indirizzo  L'indirizzo civico.
+     * @param latitudine  La latifudine geografica.
+     * @param longitudine  La longitudine geografica.
+     * @param prezzoMedio  Il prezzo medio di spesa a persona.
+     * @param delivery  <code>true</code> se offre consegna a domicilio, altrimenti <code>false</code>.
+     * @param prenotazione  <code>true</code> se accetta prenotazioni online, altrimenti <code>false</code>.
+     * @param tipoCucina  Il tipo di cucina offerta.
+     * @param stelle  La valutazione media del ristorante in stelle.
+     * @param proprietario  Il nome del proprietario del ristorante.
+     */
+    public Ristorante(int id_ristorante, String nome, String indirizzo, double latitudine,
+                      double longitudine, int prezzoMedio, boolean delivery, boolean prenotazione, String tipoCucina,
+                      Double stelle, String proprietario, int id_citta){
+        this.id_ristorante = id_ristorante;
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.latitudine = latitudine;
+        this.longitudine = longitudine;
+        this.prezzoMedio = prezzoMedio;
+        this.delivery = delivery;
+        this.prenotazione = prenotazione;
+        this.tipoCucina = tipoCucina;
+        this.stelle = stelle;
+        this.proprietario = proprietario;
+        this.id_citta = id_citta;
+    }
+
+
+    //getter
+
+    public int getId_ristorante(){ return id_ristorante;}
+
+    /**
+     * Restituisce il nome del ristorante.
+     * @return Una stringa che rappresenta il nome del ristorante.
+     */
+    public String getNome() {return nome;}
+
+    /**
+     * Restituisce l'indirizzo del ristorante.
+     * @return Una stringa che rappresenta l'indirizzo del ristorante.
+     */
+    public String getIndirizzo(){return indirizzo;}
+
+    /**
+     * Restituisce la latitudine del ristorante.
+     * @return Un valore double che rappresenta la latitudine del ristorante.
+     */
+    public double getLatitudine(){return latitudine;}
+
+    /**
+     * Restituisce la longitudine del ristorante.
+     * @return Un valore double che rappresenta la longitudine del ristorante.
+     */
+    public double getLongitudine(){return longitudine;}
+
+    /**
+     * Restituisce il prezzo medio di spesa a persona nel ristorante.
+     * @return Un valore intero che rappresenta il prezzo medio.
+     */
+    public int getPrezzoMedio(){return prezzoMedio;}
+
+    /**
+     * Restituisce se il ristorante offre servizio di consegna a domicilio.
+     * @return <code>true</code> se offre consegna a domicilio, altrimenti <code>false</code>.
+     */
+    public Boolean getDelivery(){return delivery;}
+
+    /**
+     * Restituisce se il ristorante accetta prenotazioni online.
+     * @return <code>true</code> se accetta prenotazioni online, altrimenti <code>false</code>.
+     */
+    public Boolean getPrenotazione(){return prenotazione;}
+
+    /**
+     * Restituisce il tipo di cucina offerta dal ristorante.
+     * @return Una stringa che rappresenta il tipo di cucina.
+     */
+    public String getTipoCucina(){return tipoCucina;}
+
+    /**
+     * Restituisce la valutazione media del ristorante in stelle.
+     * @return Un valore double che rappresenta la valutazione in stelle.
+     */
+    public Double getStelle(){return stelle;}
+
+    /**
+     * Restituisce il nome del proprietario del ristorante.
+     * @return Una stringa che rappresenta il nome del proprietario.
+     */
+    public String getProprietario(){return proprietario;}
+
+    public int getId_citta(){return id_citta;}
+
+    //setter
+
+   public void setId_ristorante(int idRistorante){this.id_ristorante = idRistorante;}
+
+    /**
+     * Imposta il nome del ristorante.
+     * @param nome  Il nuovo nome del ristorante.
+     */
+    public void setNome(String nome){this.nome = nome;}
+
+
+    /**
+     * Imposta l'indirizzo del ristorante.
+     * @param indirizzo  Il nuovo indirizzo del ristorante.
+     */
+    public void setIndirizzo(String indirizzo){this.indirizzo = indirizzo;}
+
+    /**
+     * Imposta la latitudine del ristorante.
+     * @param latitudine  La nuova latitudine del ristorante.
+     */
+    public void setLatitudine(double latitudine) {this.latitudine = latitudine;}
+
+    /**
+     * Imposta la longitudine del ristorante.
+     * @param longitudine  La nuova longitudine del ristorante.
+     */
+    public void setLongitudine(double longitudine){this.longitudine = longitudine;}
+
+    /**
+     * Imposta il prezzo medio di spesa a persona nel ristorante.
+     * @param prezzoMedio  Il nuovo prezzo medio.
+     */
+    public void setPrezzoMedio(int prezzoMedio){this.prezzoMedio = prezzoMedio;}
+
+    /**
+     * Imposta se il ristorante offre servizio di consegna a domicilio.
+     * @param delivery  <code>true</code> se offre consegna a domicilio, altrimenti <code>false</code>.
+     */
+    public void setDelivery(Boolean delivery) {this.delivery = delivery;}
+
+    /**
+     * Imposta se il ristorante accetta prenotazioni online.
+     * @param prenotazione  <code>true</code> se accetta prenotazioni online, altrimenti <code>false</code>.
+     */
+    public void setPrenotazione(Boolean prenotazione) {this.prenotazione = prenotazione;}
+
+    /**
+     * Imposta il tipo di cucina offerta dal ristorante.
+     * @param tipoCucina  Il nuovo tipo di cucina.
+     */
+    public void setTipoCucina(String tipoCucina){this.tipoCucina = tipoCucina;}
+
+    /**
+     * Imposta la valutazione media del ristorante in stelle.
+     * @param stelle  La nuova valutazione in stelle.
+     */
+    public void setStelle(Double stelle){this.stelle = stelle;}
+
+    /**
+     * Imposta il nome del proprietario del ristorante.
+     * @param proprietario  Il nuovo nome del proprietario.
+     */
+    public void setProprietario(String proprietario){this.proprietario = proprietario;}
+
+    public void setId_citta(int idCitta){this.id_citta = idCitta;}
+
+    /**
+     * Restituisce una rappresentazione testuale dell'oggetto Ristorante.
+     * Vengono formattati in modo leggibile i booleani e il prezzo medio.
+     *
+     * @return Una stringa con i dettagli del ristorante.
+     */
+    public String toString(){
+        return "" +"idRistorante" + id_ristorante +
+                "\nRistorante: " + nome +
+                "\nIndirizzo: " + indirizzo +
+                "\nPrezzo Medio: " + prezzoMedioZero(prezzoMedio) +
+                "\nDelivery: " + formattoSieNo(delivery) +
+                "\nPrenotazione: " + formattoSieNo(prenotazione) +
+                "\nTipo di Cucina: " + tipoCucina +
+                "\nValutazione in Stelle: " + stelle +
+                "\nidCitta" + id_citta;
+    }
+
+    /**
+     * Formatta un valore booleano in una stringa "Sì" o "No".
+     *
+     * @param valore Il booleano da convertire.
+     * @return "Non specificato" se null, "Sì" se true, "No" se false.
+     */
+    public String formattoSieNo(Boolean valore){
+        if(valore == null){
+            return "Non spcecificato";}
+        // Se è true restituisce "sì", se è false restituisce "no"
+        return valore ? "Sì" : "No";
+    }
+
+    /**
+     * Gestisce la formattazione del prezzo medio.
+     *
+     * @param prezzoMedio Il valore intero del prezzo.
+     * @return Una stringa contente il prezzo medio o "Non specificato" se è 0.
+     */
+    public String prezzoMedioZero(int prezzoMedio){
+        if(prezzoMedio == 0){
+            return "Non specificato";}
+        return Integer.toString(prezzoMedio);
+    }
+}
