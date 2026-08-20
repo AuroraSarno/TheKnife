5cc235b350dac3cbbf7510564033e4b92498c7ad
AuroraSarno <asarno1@studenti.uninsubria.it>
2026-08-20 16:41:20 +0200

    Aggiunta dei campi gestori all'interno di TheKnifeImpl

--- src/main/java/ServerTK/RMI/ServerTK.java
+++ src/main/java/ServerTK/RMI/ServerTK.java
@@ -1,5 +1,7 @@
 package ServerTK.RMI;
 
+import ServerTK.Gestori.*;
+
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -8,8 +10,13 @@ public class ServerTK {
     //metodo per avviare il server
     public void main(String [] args) {
         try {
+            GestoreCitta c = new GestoreCitta();
+            GestoreUtenti u = new GestoreUtenti();
+            GestoreRistorante r = new GestoreRistorante();
+            GestoreRecensione rr = new GestoreRecensione();
+            GestorePreferiti p = new GestorePreferiti();
             Registry registry = LocateRegistry.createRegistry(8888);
-            TheKnifeImpl theKnifeInterface = new TheKnifeImpl();
+            TheKnifeImpl theKnifeInterface = new TheKnifeImpl(c,p,rr,r,u);
             registry.rebind("TheKnife", theKnifeInterface);
         }catch (RemoteException e) {
             System.err.println("Errore all'avvio del server" + e);
--- src/main/java/ServerTK/RMI/TheKnifeImpl.java
+++ src/main/java/ServerTK/RMI/TheKnifeImpl.java
@@ -1,11 +1,23 @@
 package ServerTK.RMI;
 
+import ServerTK.Gestori.*;
+
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class TheKnifeImpl extends UnicastRemoteObject implements TheKnifeInterface {
     private static final long serialVersionUID = 1L;
-    public TheKnifeImpl() throws RemoteException{
+    private GestoreCitta gestoreCitta;
+    private GestorePreferiti gestorePreferiti;
+    private GestoreRecensione gestoreRecensione;
+    private GestoreRistorante gestoreRistorante;
+    private GestoreUtenti gestoreUtenti;
+    public TheKnifeImpl(GestoreCitta gestoreCitta, GestorePreferiti gestorePreferiti, GestoreRecensione gestoreRecensione,GestoreRistorante gestoreRistorante, GestoreUtenti gestoreUtenti) throws RemoteException{
         super();
+        this.gestoreCitta = gestoreCitta;
+        this.gestorePreferiti = gestorePreferiti;
+        this.gestoreRecensione = gestoreRecensione;
+        this.gestoreRistorante = gestoreRistorante;
+        this.gestoreUtenti = gestoreUtenti;
     }
 }
